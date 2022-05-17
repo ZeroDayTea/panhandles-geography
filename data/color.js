2253adee64a15b607b1fc34d10943da521aa7d1d
ZeroDayTea <patrick.dobranowski@gmail.com>
2022-05-16 21:52:17 -0600

    initial setup

---
+++ data/color.js
@@ -0,0 +1,614 @@
+var color = {
+  data: {
+    colorValue: {
+      thresholdMax: 60000,
+      thresholdMin: 1000
+    }
+  },
+  applyData: 'colorValue',
+  values: {
+    AF: {
+      colorValue: 1958,
+    },
+    AL: {
+      colorValue: 12507,
+    },
+    DZ: {
+      colorValue: 15237,
+    },
+    AO: {
+      colorValue: 6753,
+    },
+    AG: {
+      colorValue: 26231,
+    },
+    AR: {
+      colorValue: 20876,
+    },
+    AM: {
+      colorValue: 9456,
+    },
+    AU: {
+      colorValue: 50334,
+    },
+    AT: {
+      colorValue: 49869,
+    },
+    AZ: {
+      colorValue: 17492,
+    },
+    BS: {
+      colorValue: 31139,
+    },
+    BH: {
+      colorValue: 48505,
+    },
+    BD: {
+      colorValue: 4211,
+    },
+    BB: {
+      colorValue: 18664,
+    },
+    BY: {
+      colorValue: 18931,
+    },
+    BE: {
+      colorValue: 46553,
+    },
+    BZ: {
+      colorValue: 8324,
+    },
+    BJ: {
+      colorValue: 2277,
+    },
+    BT: {
+      colorValue: 8744,
+    },
+    BO: {
+      colorValue: 7547,
+    },
+    BA: {
+      colorValue: 12724,
+    },
+    BW: {
+      colorValue: 17828,
+    },
+    BR: {
+      colorValue: 15602,
+    },
+    BN: {
+      colorValue: 78196,
+    },
+    BG: {
+      colorValue: 21687,
+    },
+    BF: {
+      colorValue: 1889,
+    },
+    BI: {
+      colorValue: 735,
+    },
+    KH: {
+      colorValue: 4012,
+    },
+    CM: {
+      colorValue: 3660,
+    },
+    CA: {
+      colorValue: 48265,
+    },
+    XK: {
+      colorValue: 10514,
+    },
+    CV: {
+      colorValue: 6944,
+    },
+    CF: {
+      colorValue: 677,
+    },
+    TD: {
+      colorValue: 2344,
+    },
+    CL: {
+      colorValue: 24537,
+    },
+    CN: {
+      colorValue: 16660,
+    },
+    CO: {
+      colorValue: 14485,
+    },
+    KM: {
+      colorValue: 1588,
+    },
+    CG: {
+      colorValue: 6642,
+    },
+    CR: {
+      colorValue: 16877,
+    },
+    HR: {
+      colorValue: 24423,
+    },
+    CY: {
+      colorValue: 37023,
+    },
+    CZ: {
+      colorValue: 35512,
+    },
+    CD: {
+      colorValue: 790,
+    },
+    DK: {
+      colorValue: 49883,
+    },
+    DJ: {
+      colorValue: 3559,
+    },
+    DM: {
+      colorValue: 11102,
+    },
+    DO: {
+      colorValue: 16944,
+    },
+    EC: {
+      colorValue: 11482,
+    },
+    EG: {
+      colorValue: 12671,
+    },
+    SV: {
+      colorValue: 8948,
+    },
+    GQ: {
+      colorValue: 36017,
+    },
+    ER: {
+      colorValue: 1581,
+    },
+    EE: {
+      colorValue: 31749,
+    },
+    ET: {
+      colorValue: 2161,
+    },
+    FM: {
+      colorValue: 3393,
+    },
+    FJ: {
+      colorValue: 9777,
+    },
+    FI: {
+      colorValue: 44333,
+    },
+    FR: {
+      colorValue: 43760,
+    },
+    GA: {
+      colorValue: 19254,
+    },
+    GM: {
+      colorValue: 1713,
+    },
+    GE: {
+      colorValue: 10747,
+    },
+    DE: {
+      colorValue: 50425,
+    },
+    GH: {
+      colorValue: 4729,
+    },
+    GR: {
+      colorValue: 27737,
+    },
+    GD: {
+      colorValue: 14926,
+    },
+    GT: {
+      colorValue: 8145,
+    },
+    GN: {
+      colorValue: 2041,
+    },
+    GW: {
+      colorValue: 1845,
+    },
+    GY: {
+      colorValue: 8161,
+    },
+    HT: {
+      colorValue: 1815,
+    },
+    HN: {
+      colorValue: 5562,
+    },
+    HK: {
+      colorValue: 61393,
+    },
+    HU: {
+      colorValue: 29474,
+    },
+    IS: {
+      colorValue: 51841,
+    },
+    IN: {
+      colorValue: 7183,
+    },
+    ID: {
+      colorValue: 12377,
+    },
+    IR: {
+      colorValue: 20200,
+    },
+    IQ: {
+      colorValue: 16954,
+    },
+    IE: {
+      colorValue: 75539,
+    },
+    IL: {
+      colorValue: 36340,
+    },
+    IT: {
+      colorValue: 38140,
+    },
+    CI: {
+      colorValue: 3883,
+    },
+    JM: {
+      colorValue: 9163,
+    },
+    JP: {
+      colorValue: 42832,
+    },
+    JO: {
+      colorValue: 12494,
+    },
+    KZ: {
+      colorValue: 26252,
+    },
+    KE: {
+      colorValue: 3491,
+    },
+    KI: {
+      colorValue: 1976,
+    },
+    KW: {
+      colorValue: 66163,
+    },
+    KG: {
+      colorValue: 3667,
+    },
+    LA: {
+      colorValue: 7366,
+    },
+    LV: {
+      colorValue: 27644,
+    },
+    LB: {
+      colorValue: 19439,
+    },
+    LS: {
+      colorValue: 3581,
+    },
+    LR: {
+      colorValue: 1354,
+    },
+    LY: {
+      colorValue: 9987,
+    },
+    LT: {
+      colorValue: 32299,
+    },
+    LU: {
+      colorValue: 106374,
+    },
+    MO: {
+      colorValue: 111629,
+    },
+    MK: {
+      colorValue: 14914,
+    },
+    MG: {
+      colorValue: 1551,
+    },
+    MW: {
+      colorValue: 1167,
+    },
+    MY: {
+      colorValue: 29041,
+    },
+    MV: {
+      colorValue: 19151,
+    },
+    ML: {
+      colorValue: 2170,
+    },
+    MT: {
+      colorValue: 41945,
+    },
+    MH: {
+      colorValue: 3439,
+    },
+    MR: {
+      colorValue: 4444,
+    },
+    MU: {
+      colorValue: 21640,
+    },
+    MX: {
+      colorValue: 19903,
+    },
+    MD: {
+      colorValue: 5661,
+    },
+    MN: {
+      colorValue: 12979,
+    },
+    ME: {
+      colorValue: 17735,
+    },
+    MA: {
+      colorValue: 8567,
+    },
+    MZ: {
+      colorValue: 1244,
+    },
+    MM: {
+      colorValue: 6244,
+    },
+    NA: {
+      colorValue: 11312,
+    },
+    NR: {
+      colorValue: 12002,
+    },
+    NP: {
+      colorValue: 2679,
+    },
+    NL: {
+      colorValue: 53635,
+    },
+    NZ: {
+      colorValue: 38934,
+    },
+    NI: {
+      colorValue: 5849,
+    },
+    NE: {
+      colorValue: 1164,
+    },
+    NG: {
+      colorValue: 5929,
+    },
+    NO: {
+      colorValue: 71831,
+    },
+    OM: {
+      colorValue: 45157,
+    },
+    PK: {
+      colorValue: 5358,
+    },
+    PW: {
+      colorValue: 15934,
+    },
+    PA: {
+      colorValue: 25351,
+    },
+    PG: {
+      colorValue: 3674,
+    },
+    PY: {
+      colorValue: 9826,
+    },
+    PE: {
+      colorValue: 13334,
+    },
+    PH: {
+      colorValue: 8315,
+    },
+    PL: {
+      colorValue: 29521,
+    },
+    PT: {
+      colorValue: 30417,
+    },
+    PR: {
+      colorValue: 37339,
+    },
+    QA: {
+      colorValue: 124529,
+    },
+    RO: {
+      colorValue: 24508,
+    },
+    RU: {
+      colorValue: 27834,
+    },
+    RW: {
+      colorValue: 2080,
+    },
+    KN: {
+      colorValue: 26845,
+    },
+    LC: {
+      colorValue: 14450,
+    },
+    VC: {
+      colorValue: 11491,
+    },
+    WS: {
+      colorValue: 5740,
+    },
+    SM: {
+      colorValue: 59466,
+    },
+    ST: {
+      colorValue: 3180,
+    },
+    SA: {
+      colorValue: 54777,
+    },
+    SN: {
+      colorValue: 2727,
+    },
+    RS: {
+      colorValue: 15000,
+    },
+    SC: {
+      colorValue: 28779,
+    },
+    SL: {
+      colorValue: 1553,
+    },
+    SG: {
+      colorValue: 93906,
+    },
+    SK: {
+      colorValue: 33025,
+    },
+    SI: {
+      colorValue: 34408,
+    },
+    SB: {
+      colorValue: 2157,
+    },
+    SO: {
+      colorValue: 1229,
+    },
+    ZA: {
+      colorValue: 13545,
+    },
+    KR: {
+      colorValue: 39434,
+    },
+    SS: {
+      colorValue: 1489,
+    },
+    ES: {
+      colorValue: 38286,
+    },
+    LK: {
+      colorValue: 12811,
+    },
+    SD: {
+      colorValue: 4586,
+    },
+    SR: {
+      colorValue: 13876,
+    },
+    SZ: {
+      colorValue: 9884,
+    },
+    SE: {
+      colorValue: 51475,
+    },
+    CH: {
+      colorValue: 61422,
+    },
+    TW: {
+      colorValue: 50294,
+    },
+    TJ: {
+      colorValue: 3211,
+    },
+    TZ: {
+      colorValue: 3240,
+    },
+    TH: {
+      colorValue: 17855,
+    },
+    TL: {
+      colorValue: 5444,
+    },
+    TG: {
+      colorValue: 1659,
+    },
+    TO: {
+      colorValue: 5608,
+    },
+    TT: {
+      colorValue: 31367,
+    },
+    TN: {
+      colorValue: 11755,
+    },
+    TR: {
+      colorValue: 26893,
+    },
+    TM: {
+      colorValue: 18126,
+    },
+    TV: {
+      colorValue: 3807,
+    },
+    UG: {
+      colorValue: 2354,
+    },
+    UA: {
+      colorValue: 8713,
+    },
+    AE: {
+      colorValue: 67741,
+    },
+    GB: {
+      colorValue: 44117,
+    },
+    US: {
+      colorValue: 59501,
+    },
+    UY: {
+      colorValue: 22371,
+    },
+    UZ: {
+      colorValue: 6929,
+    },
+    VU: {
+      colorValue: 2739,
+    },
+    VE: {
+      colorValue: 12114,
+    },
+    VN: {
+      colorValue: 6913,
+    },
+    YE: {
+      colorValue: 1287,
+    },
+    ZM: {
+      colorValue: 3996,
+    },
+    ZW: {
+      colorValue: 2283,
+    },
+    GL: {
+      colorValue: 4000,
+    },
+    GF: {
+      colorValue: 4000,
+    },
+    KP: {
+      colorValue: 4000,
+    },
+    PS: {
+      colorValue: 4000,
+    },
+    EH: {
+      colorValue: 4000,
+    },
+    AD: {
+      colorValue: 4000,
+    },
+    VA: {
+      colorValue: 4000,
+    },
+    MC: {
+      colorValue: 4000,
+    },
+    SY: {
+      colorValue: 4000,
+    }
+  }
+};
---
+++ main.js
@@ -0,0 +1,446 @@
+var countriesDict = {
+    AF: 'Afghanistan',
+    AX: 'Åland Islands',
+    AL: 'Albania',
+    DZ: 'Algeria',
+    AS: 'American Samoa',
+    AD: 'Andorra',
+    AO: 'Angola',
+    AI: 'Anguilla',
+    AQ: 'Antarctica',
+    AG: 'Antigua and Barbuda',
+    AR: 'Argentina',
+    AM: 'Armenia',
+    AW: 'Aruba',
+    AU: 'Australia',
+    AT: 'Austria',
+    AZ: 'Azerbaijan',
+    BS: 'Bahamas',
+    BH: 'Bahrain',
+    BD: 'Bangladesh',
+    BB: 'Barbados',
+    BY: 'Belarus',
+    BE: 'Belgium',
+    BZ: 'Belize',
+    BJ: 'Benin',
+    BM: 'Bermuda',
+    BT: 'Bhutan',
+    BO: 'Bolivia',
+    BA: 'Bosnia and Herzegovina',
+    BW: 'Botswana',
+    BR: 'Brazil',
+    IO: 'British Indian Ocean Territory',
+    VG: 'British Virgin Islands',
+    BN: 'Brunei Darussalam',
+    BG: 'Bulgaria',
+    BF: 'Burkina Faso',
+    BI: 'Burundi',
+    KH: 'Cambodia',
+    CM: 'Cameroon',
+    CA: 'Canada',
+    CV: 'Cape Verde',
+    BQ: 'Caribbean Netherlands',
+    KY: 'Cayman Islands',
+    CF: 'Central African Republic',
+    TD: 'Chad',
+    CL: 'Chile',
+    CN: 'China',
+    CX: 'Christmas Island',
+    CC: 'Cocos Islands',
+    CO: 'Colombia',
+    KM: 'Comoros',
+    CG: 'Congo',
+    CK: 'Cook Islands',
+    CR: 'Costa Rica',
+    HR: 'Croatia',
+    CU: 'Cuba',
+    CW: 'Curaçao',
+    CY: 'Cyprus',
+    CZ: 'Czech Republic',
+    CD: 'Democratic Republic of the Congo',
+    DK: 'Denmark',
+    DJ: 'Djibouti',
+    DM: 'Dominica',
+    DO: 'Dominican Republic',
+    EC: 'Ecuador',
+    EG: 'Egypt',
+    SV: 'El Salvador',
+    GQ: 'Equatorial Guinea',
+    ER: 'Eritrea',
+    EE: 'Estonia',
+    ET: 'Ethiopia',
+    FK: 'Falkland Islands',
+    FO: 'Faroe Islands',
+    FM: 'Federated States of Micronesia',
+    FJ: 'Fiji',
+    FI: 'Finland',
+    FR: 'France',
+    GF: 'French Guiana',
+    PF: 'French Polynesia',
+    TF: 'French Southern Territories',
+    GA: 'Gabon',
+    GM: 'Gambia',
+    GE: 'Georgia',
+    DE: 'Germany',
+    GH: 'Ghana',
+    GI: 'Gibraltar',
+    GR: 'Greece',
+    GL: 'Greenland',
+    GD: 'Grenada',
+    GP: 'Guadeloupe',
+    GU: 'Guam',
+    GT: 'Guatemala',
+    GN: 'Guinea',
+    GW: 'Guinea-Bissau',
+    GY: 'Guyana',
+    HT: 'Haiti',
+    HN: 'Honduras',
+    HK: 'Hong Kong',
+    HU: 'Hungary',
+    IS: 'Iceland',
+    IN: 'India',
+    ID: 'Indonesia',
+    IR: 'Iran',
+    IQ: 'Iraq',
+    IE: 'Ireland',
+    IM: 'Isle of Man',
+    IL: 'Israel',
+    IT: 'Italy',
+    CI: 'Ivory Coast',
+    JM: 'Jamaica',
+    JP: 'Japan',
+    JE: 'Jersey',
+    JO: 'Jordan',
+    KZ: 'Kazakhstan',
+    KE: 'Kenya',
+    KI: 'Kiribati',
+    XK: 'Kosovo',
+    KW: 'Kuwait',
+    KG: 'Kyrgyzstan',
+    LA: 'Laos',
+    LV: 'Latvia',
+    LB: 'Lebanon',
+    LS: 'Lesotho',
+    LR: 'Liberia',
+    LY: 'Libya',
+    LI: 'Liechtenstein',
+    LT: 'Lithuania',
+    LU: 'Luxembourg',
+    MO: 'Macau',
+    MK: 'Macedonia',
+    MG: 'Madagascar',
+    MW: 'Malawi',
+    MY: 'Malaysia',
+    MV: 'Maldives',
+    ML: 'Mali',
+    MT: 'Malta',
+    MH: 'Marshall Islands',
+    MQ: 'Martinique',
+    MR: 'Mauritania',
+    MU: 'Mauritius',
+    YT: 'Mayotte',
+    MX: 'Mexico',
+    MD: 'Moldova',
+    MC: 'Monaco',
+    MN: 'Mongolia',
+    ME: 'Montenegro',
+    MS: 'Montserrat',
+    MA: 'Morocco',
+    MZ: 'Mozambique',
+    MM: 'Myanmar',
+    NA: 'Namibia',
+    NR: 'Nauru',
+    NP: 'Nepal',
+    NL: 'Netherlands',
+    NC: 'New Caledonia',
+    NZ: 'New Zealand',
+    NI: 'Nicaragua',
+    NE: 'Niger',
+    NG: 'Nigeria',
+    NU: 'Niue',
+    NF: 'Norfolk Island',
+    KP: 'North Korea',
+    MP: 'Northern Mariana Islands',
+    NO: 'Norway',
+    OM: 'Oman',
+    PK: 'Pakistan',
+    PW: 'Palau',
+    PS: 'Palestine',
+    PA: 'Panama',
+    PG: 'Papua New Guinea',
+    PY: 'Paraguay',
+    PE: 'Peru',
+    PH: 'Philippines',
+    PN: 'Pitcairn Islands',
+    PL: 'Poland',
+    PT: 'Portugal',
+    PR: 'Puerto Rico',
+    QA: 'Qatar',
+    RE: 'Reunion',
+    RO: 'Romania',
+    RU: 'Russia',
+    RW: 'Rwanda',
+    SH: 'Saint Helena',
+    KN: 'Saint Kitts and Nevis',
+    LC: 'Saint Lucia',
+    PM: 'Saint Pierre and Miquelon',
+    VC: 'Saint Vincent and the Grenadines',
+    WS: 'Samoa',
+    SM: 'San Marino',
+    ST: 'São Tomé and Príncipe',
+    SA: 'Saudi Arabia',
+    SN: 'Senegal',
+    RS: 'Serbia',
+    SC: 'Seychelles',
+    SL: 'Sierra Leone',
+    SG: 'Singapore',
+    SX: 'Sint Maarten',
+    SK: 'Slovakia',
+    SI: 'Slovenia',
+    SB: 'Solomon Islands',
+    SO: 'Somalia',
+    ZA: 'South Africa',
+    GS: 'South Georgia and the South Sandwich Islands',
+    KR: 'South Korea',
+    SS: 'South Sudan',
+    ES: 'Spain',
+    LK: 'Sri Lanka',
+    SD: 'Sudan',
+    SR: 'Suriname',
+    SJ: 'Svalbard and Jan Mayen',
+    SZ: 'Eswatini',
+    SE: 'Sweden',
+    CH: 'Switzerland',
+    SY: 'Syria',
+    TW: 'Taiwan',
+    TJ: 'Tajikistan',
+    TZ: 'Tanzania',
+    TH: 'Thailand',
+    TL: 'Timor-Leste',
+    TG: 'Togo',
+    TK: 'Tokelau',
+    TO: 'Tonga',
+    TT: 'Trinidad and Tobago',
+    TN: 'Tunisia',
+    TR: 'Turkey',
+    TM: 'Turkmenistan',
+    TC: 'Turks and Caicos Islands',
+    TV: 'Tuvalu',
+    UG: 'Uganda',
+    UA: 'Ukraine',
+    AE: 'United Arab Emirates',
+    GB: 'United Kingdom',
+    US: 'United States',
+    UM: 'United States Minor Outlying Islands',
+    VI: 'United States Virgin Islands',
+    UY: 'Uruguay',
+    UZ: 'Uzbekistan',
+    VU: 'Vanuatu',
+    VA: 'Vatican City',
+    VE: 'Venezuela',
+    VN: 'Vietnam',
+    WF: 'Wallis and Futuna',
+    EH: 'Western Sahara',
+    YE: 'Yemen',
+    ZM: 'Zambia',
+    ZW: 'Zimbabwe'
+};
+
+panhandlesDict = {
+    CP: 'Cibitoke Province, Burundi',
+    FC: 'Far North Region, Cameroon',
+    CC: 'Congo Pedicle, Democratic Republic of the Congo',
+    KC: 'Kongo Central, Democratic Republic of the Congo',
+    SR: 'Southern Red Sea Region, Eritrea',
+    GR: 'Gambela Region, Ethiopia',
+    SE: 'Somali Region, Ethiopia',
+    NC: 'Nimba County, Liberia',
+    ND: 'Nsanje District, Malawi',
+    KR: 'Kayes Region, Mali',
+    AM: 'Azawad, Mali',
+    TP: 'Tete Province, Mozambique',
+    CN: 'Caprivi Strip, Namibia',
+    CS: 'Casamance, Senegal',
+    KG: 'Kalahari Gemsbok National Park, South Africa',
+    UN: 'Upper Nile, South Sudan',
+    BN: 'Blue Nile, Sudan',
+    WH: 'Wadi Halfa Salient, Sudan',
+    KT: 'Kagera Region, Tanzania'
+}
+
+const countryMapper = (country) => countriesDict[country] || "No Country of that ID";
+var timeInterval;
+
+var changeGameMode = function(selectedObject) {
+    resetTime();
+
+    if(selectedObject.value == "world")
+    {
+        startWorldGeography();
+    }
+    else if(selectedObject.value == "panhandles")
+    {
+        startPanhandlesGeography();
+    }
+    else if(selectedObject.value == "explore")
+    {
+        startExploreGeography();
+    }
+}
+
+//variables shared by two game modes
+var currentGameMode = "none";
+var goal = ""
+var score = 0;
+countriesToGuess = [];
+
+//method to start and play world geography
+function startWorldGeography() {
+    document.getElementById("panhandlesMap").innerHTML = "";
+    new svgMap({
+        targetElementID: 'panhandlesMap',
+        hideFlag: true,
+        hideToolTip: true,
+        data: color,
+        mouseWheelZoomEnabled: true,
+        mouseWheelZoomWithKey: true
+      });
+
+    currentGameMode = "world";
+    goal = "";
+    score = 0;
+    countriesToGuess = [];
+    updateScore();
+
+    for (var key in countriesDict) {
+        var val = countriesDict[key];
+        countriesToGuess.push(val);
+    }
+
+    chooseRandomCountry();
+
+    timeInterval = setInterval(setTime, 1000);
+}
+
+//method to start and play panhandles geography
+function startPanhandlesGeography() {
+    document.getElementById("panhandlesMap").innerHTML = "";
+    new svgMap({
+        targetElementID: 'panhandlesMap',
+        hideFlag: true,
+        hideToolTip: true,
+        data: color,
+        mouseWheelZoomEnabled: true,
+        mouseWheelZoomWithKey: true
+      });
+
+    currentGameMode = "panhandles";
+    goal = "";
+    score = 0;
+    countriesToGuess = [];
+    updateScore();
+    
+    timeInterval = setInterval(setTime, 1000);
+}
+
+//method to start and play panhandles geography
+function startExploreGeography() {
+    document.getElementById("panhandlesMap").innerHTML = "";
+    new svgMap({
+        targetElementID: 'panhandlesMap',
+        data: color,
+        mouseWheelZoomEnabled: true,
+        mouseWheelZoomWithKey: true
+      });
+
+    currentGameMode = "explore";
+    goal = "";
+    score = 0;
+    countriesToGuess = [];
+    updateScore();
+}
+
+function chooseRandomCountry() {
+    goal = countriesToGuess[Math.floor(Math.random()*countriesToGuess.length)];
+    document.getElementById('goal').innerHTML = "Click on " + goal;
+}
+
+var countryClick = function(countryid) {
+    var countryClicked = countryMapper(countryid);
+    if(currentGameMode == "world")
+    {
+        if(countryClicked == goal)
+        {
+            score += 1;
+            var index = countriesToGuess.indexOf(countryClicked);
+            if(index > -1)
+            {
+                countriesToGuess.splice(index, 1);
+            }
+            chooseRandomCountry();
+            updateScore();
+        }
+        else
+        {
+            score -= 1;
+            updateScore();
+        }
+    }
+    else if(currentGameMode == "panhandles")
+    {
+        
+    }
+}
+
+//change score html element to current score variable
+function updateScore()
+{
+    if(currentGameMode == "world")
+    {
+        document.getElementById("score").innerHTML = "Score: " + score + "/" + Object.keys(countriesDict).length;
+    }
+    else if(currentGameMode == "panhandles")
+    {
+        document.getElementById("score").innerHTML = "Score: " + score + "/" + Object.keys(panhandlesDict).length;
+    }
+}
+
+//move goal with mouse
+(function() {
+    document.onmousemove = handleMouseMove;
+    function handleMouseMove(event) {
+        event = event || window.event;
+
+        // Use event.pageX / event.pageY here
+        document.getElementById('goal').style.left = `${event.pageX}px`;
+        document.getElementById('goal').style.top = `${event.pageY}px`;
+    }
+})();
+
+//timer variables
+var minutesLabel = document.getElementById("minutes");
+var secondsLabel = document.getElementById("seconds");
+var totalSeconds = 0;
+//methods to update time
+function setTime() {
+    totalSeconds++;
+    secondsLabel.innerHTML = pad(totalSeconds % 60);
+    minutesLabel.innerHTML = pad(parseInt(totalSeconds / 60));
+    console.log(totalSeconds);
+  }
+  
+  function pad(val) {
+    var valString = val + "";
+    if (valString.length < 2) {
+      return "0" + valString;
+    } else {
+      return valString;
+    }
+  }
+
+function resetTime(){
+    totalSeconds = 0;
+    secondsLabel.innerHTML = "00";
+    minutesLabel.innerHTML = "00";
+    clearInterval(timeInterval);
+}
